e323d9e19ae132e3dbfb7f4d8240f3e001b234c6
Varya <trifonovavarya@gmail.com>
2024-10-01 09:09:08 +0500

    Стих готов

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите длину первого катета:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите длину второго катета:");
+            double b = double.Parse(Console.ReadLine());
+
+            double c = Math.Sqrt(a * a + b * b);
+
+            double height = (a * b) / c;
+
+            Console.WriteLine($"Длина высоты, опущенной из вершины прямого угла на гипотенузу: {height}"); 
+
+            Console.ReadKey();
+        }
+    }
+}
